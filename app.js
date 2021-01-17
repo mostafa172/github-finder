b15811af206d6898bc53ecb6ac0cfb2646d9318f
Mostafa Hazem <mostafa17_2@live.com>
2021-01-18 00:56:51 +0200

    show profile and public repos

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // Init Github
 const github = new Github;
 
+//Init UI
+const ui = new UI;
+
 // Search Input
 const searchUser = document.getElementById('searchUser');
+const searchButton = document.getElementById('searchButton');
 
-// Search Input Event Listener
-searchUser.addEventListener('keyup', (e) => {
+// Search Event Listener
+searchButton.addEventListener('click', () => {
   // Get Input Text
-  const userText = e.target.value;
+  const userText = searchButton.parentElement.previousElementSibling.value;
+  console.log(userText);
 
   if(userText !== ''){
     // Make http call
@@ -15,12 +20,39 @@ searchUser.addEventListener('keyup', (e) => {
       .then(data => {
         if(data.profile.message === 'Not Found') {
           // Show Alert 
-
+          ui.showAlert('User not found', 'alert alert-danger');
         } else {
           // Show Profile
+          ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
         }
       })
   } else {
     // Clear Profile
+    ui.clearProfile();
   }
 });
+
+// Search Input Event Listener
+// searchUser.addEventListener('keyup', (e) => {
+//   // Get Input Text
+//   const userText = e.target.value;
+
+//   if(userText !== ''){
+//     // Make http call
+//     github.getUser(userText)
+//       .then(data => {
+//         if(data.profile.message === 'Not Found') {
+//           // Show Alert 
+//           ui.showAlert('User not found', 'alert alert-danger');
+//         } else {
+//           // Show Profile
+//           ui.showProfile(data.profile);
+//           ui.showRepos(data.repos);
+//         }
+//       })
+//   } else {
+//     // Clear Profile
+//     ui.clearProfile();
+//   }
+// });
--- github.js
+++ github.js
@@ -1,16 +1,25 @@
 class Github {
   constructor() {
-    this.client_id = '3da1ece301bfce16f772';
-    this.client_secret = '4c6c798059d1238a531694fa79b18ee1d6b35303';
+    this.client_id = '7e6784ab646694d44dfb';
+    this.client_secret = 'd65f842f380cfa4874ca2fefcbf0ad86a691b886';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    // const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+
+    // const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`);
 
     const profile = await profileResponse.json();
-
+    const repos = await repoResponse.json();
+    
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
